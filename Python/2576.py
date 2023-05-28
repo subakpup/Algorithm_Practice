cf0e3ffe4d7bfbb2e89b68a36fb118b360bb9e1b
hjsu0825 <hjsu0825@naver.com>
2023-05-29 02:45:42 +0900

    230529

--- Python/2576.py
+++ Python/2576.py
@@ -1,5 +1,10 @@
-import sys
-input = sys.stdin.readline
-
+arr = []
 for i in range(7):
     n = int(input())
+    if n % 2 == 1:
+        arr.append(n)
+if not arr:
+    print(-1)
+else:
+    print(sum(arr))
+    print(min(arr))
