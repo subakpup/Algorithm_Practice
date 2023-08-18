1f74724659fdc0bb0619ae5add4c8abcb0dc7917
hjsu0825 <hjsu0825@naver.com>
2023-08-18 21:31:07 +0900

    230818

--- Python/quiz.py
+++ Python/quiz.py
@@ -29,17 +29,44 @@
 
 ########################################
 
-class Book:
+# class Book:
 
-    def setData(self, title, price, author):
-        self.title = title
+#     def setData(self, title, price, author):
+#         self.title = title
+#         self.price = price
+#         self.author = author
+
+#     def printData(self):
+#         print('제목 : ', self.title)
+#         print('가격 : ', self.price)
+#         print('저자 : ', self.author)
+
+#     def __init__(self):
+#         print('책 객체를 새로 만들었어요~')
+
+################################################3
+class House:
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
         self.price = price
-        self.author = author
+        self.completion_year = completion_year
 
-    def printData(self):
-        print('제목 : ', self.title)
-        print('가격 : ', self.price)
-        print('저자 : ', self.author)
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
 
-    def __init__(self):
-        print('책 객체를 새로 만들었어요~')
+
+houses = []
+house1 = House('강남','아파트','매매','10억','2010년')
+house2 = House('마포','오피스텔','전세','5억','2007년')
+house3 = House('송파','빌라','월세','500/50','2000년')
+
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print(f'총 {len(houses)}의 매물이 있습니다.')
+
+for house in houses:
+    house.show_detail()
