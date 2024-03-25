b1af7efc9a23dbf1bcae2e0c713cbf8b84d14f15
hjsu0825 <hjsu0825@naver.com>
2024-03-26 02:06:43 +0900

    240325

--- Python/17413.py
+++ Python/17413.py
@@ -1,9 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-s = input().split()
+stk = []
+s = input().rstrip()
+chk = False
+res = ''
 
-for i in range(len(s)):
-    for j in s[i]:
-        print((j[::-1]), end=' ')
-    print()
+for i in s:
+    if i == " ":
+        while stk:
+            res += stk.pop()
+        res += i
+    elif i == "<":
+        while stk:
+            res += stk.pop()
+        chk = True
+        res += i
+    elif i == ">":
+        chk = False
+        res += i
+    elif chk:
+        res += i
+    else:
+        stk.append(i)
+        
+while stk:
+    res += stk.pop()
+print(res)
