b74755e4a4a56c9a1ae416d3c301df36e9056f31
hjsu0825 <hjsu0825@naver.com>
2024-04-16 01:04:37 +0900

    hmm..

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-from collections import deque
-print(deque(input().replace(',','').replace('[','').replace(']','')))
-=======
-a,b = map(int,input().split())
+import sys, random
+input = sys.stdin.readline
 
-res = a//b
-print(res) +1
->>>>>>> 0a60c5d977e6abab282d91023629e47b5ebc73be
+arr = []
+random.shuffle(arr)
+
+print(*arr, sep='')
