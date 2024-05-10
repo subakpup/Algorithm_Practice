8ab3478c27b8958306a9ccb039196cc4a9689132
hjsu0825 <hjsu0825@naver.com>
2024-05-10 22:26:09 +0900

    240509

--- Python/1676.py
+++ Python/1676.py
@@ -1,3 +1,19 @@
 import sys
 input = sys.stdin.readline
 
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+n = int(input())
+arr = list(str(factorial(n)))
+res = 0
+
+for i in range(1, len(arr)+1):
+    if arr[-i] == '0':
+        res += 1
+    else:
+        print(res)
+        break
