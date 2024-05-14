9f7423bdd2e018502276c66ae053b5bea19ca3b7
hjsu0825 <hjsu0825@naver.com>
2024-05-14 10:51:16 +0900

    240513

---
+++ Python/27433.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(int(input())))
