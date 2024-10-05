686454354d1d2d150fa1a2f2df26a9944e5a43c8
hjsu0825 <hjsu0825@naver.com>
2024-10-06 01:03:48 +0900

    241005

---
+++ Python/2712.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    n, s = input().split()
+    if s == "kg":
+        print(f'{float(n)*2.2046:.4f} lb')
+    elif s == "lb":
+        print(f'{float(n)*0.4536:.4f} kg')
+    elif s == "l":
+        print(f'{float(n)*0.2642:.4f} g')
+    elif s == "g":
+        print(f'{float(n)*3.7854:.4f} l')
