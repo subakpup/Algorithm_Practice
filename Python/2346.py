566d2877656a43cdf80ddc9ac24d19f90e42e4aa
hjsu0825 <hjsu0825@naver.com>
2024-10-12 20:34:05 +0900

    241011

--- Python/2346.py
+++ Python/2346.py
@@ -3,3 +3,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+queue = deque(enumerate(map(int,input().split())))
+
+for i in range(n):
+    a = queue.popleft()
+    print(a[0]+1, end=' ')
+    if a[1] > 0:
+        queue.rotate(-(a[1]-1))
+    else:
+        queue.rotate(-a[1])
---
+++ Python/24511.py
@@ -0,0 +1,8 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+b = deque(map(int,input().split()))
+m = int(input())
