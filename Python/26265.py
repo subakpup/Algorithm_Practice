2eb3ee8b68d51a8a9b550a371035c14b4d1396ae
hjsu0825 <hjsu0825@naver.com>
2024-10-15 14:55:36 +0900

    241014

--- Python/26265.py
+++ Python/26265.py
@@ -1,8 +1,18 @@
 import sys
-input = sys.stdin.readline
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(((sys.stdin.readline().split())))
+arr.sort()
+arr2 = {}
+for i in arr:
+    if i[0] in arr2.keys():
+        arr2[i[0]].append(i[1])
+    else:
+        arr2[i[0]] = [i[1]]
+for i in arr2:
+    arr2[i].sort(reverse = True)
 
-dic = dict()
-for i in range(int(input())):
-    a,b = input().split()
-    dic[a] = b
-print(dic)
+for i in arr2:
+    for j in arr2[i]:
+        print(i, j)
