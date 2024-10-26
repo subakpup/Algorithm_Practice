c6dc22b62e703129f64412d51f96c36c22e9dc7b
hjsu0825 <hjsu0825@naver.com>
2024-10-27 01:26:45 +0900

    241026

--- Python/15663.py
+++ Python/15663.py
@@ -2,17 +2,22 @@ import sys
 input = sys.stdin.readline
 
 def dfs():
+    chk = 0
     if len(s) == m:
         print(*s)
         return
     for i in range(n):
-        if arr[i] not in s:
+        if not visited[i] and chk != arr[i]:
+            visited[i] = True
             s.append(arr[i])
+            chk = arr[i]
             dfs()
+            visited[i] = False
             s.pop()
 
 if __name__ == '__main__':
     n,m = map(int,input().split())
     arr = sorted(map(int,input().split()))
     s = []
+    visited = [False] * n
     dfs()
