75579c87766480ee6e456396faa03f200af25ca1
hjsu0825 <hjsu0825@naver.com>
2024-12-07 13:30:41 +0900

    241125

--- Python/11729.py
+++ Python/11729.py
@@ -1,10 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def func(n):
+def func(n, start, end):
     if n == 1:
-        return 1
+        print(start, end)
+        return
+    
+    func(n-1, start, 6-start-end)
+    print(start, end)
+    func(n-1, 6-start-end, end)
     
 
-n = int(input())
-res = []
+if __name__ == '__main__':
+    n = int(input())
+    print(2**n-1)
+    func(n,1,3)
