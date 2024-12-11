7271eab084cfb2abb91d3790765f96b0b6a5c0bc
hjsu0825 <hjsu0825@naver.com>
2024-12-11 10:54:08 +0900

    241210

--- Python/1912.py
+++ Python/1912.py
@@ -1,7 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-if __name__ == "__main__":
-    n = int(input())
-    arr = list(map(int,input().split()))
-    
+n = int(input())
+arr = list(map(int,input().split()))
+dp = [0] * n
+dp[0] = arr[0]
+for i in range(1,n):
+    dp[i] = max(arr[i], arr[i]+dp[i-1])
+print(max(dp))
