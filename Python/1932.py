fb121a901087b7e086798874f3cf4ebf88db2158
hjsu0825 <hjsu0825@naver.com>
2024-12-16 16:48:12 +0900

    241213

--- Python/1932.py
+++ Python/1932.py
@@ -1,15 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def func(n,arr):
-    for i in range(n):
-        dp[i] = dp[i-1] + max(arr[i])
-        print(max(arr[i]))
+def func(n):
+    for i in range(1,n):
+        for j in range(i+1):
+            if j == 0:
+                dp[i][j] += dp[i-1][j]
+            elif j == i:
+                dp[i][j] += dp[i-1][j-1]
+            else:
+                dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
 
 if __name__ == "__main__":
     n = int(input())
-    arr = [list(map(int,input().split())) for _ in range(n)]
-    dp = [0] * n
-    dp[0] = arr[0]
-    func(n,arr)
-    print(dp)
+    dp = [list(map(int,input().split())) for _ in range(n)]
+    func(n)
+    print(max(dp[-1]))
