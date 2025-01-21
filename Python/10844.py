efa4687a5d22374ccc1f001057e079c19e6576d1
subakpup <129526142+subakpup@users.noreply.github.com>
2025-01-21 16:20:20 +0900

    250120

--- Python/10844.py
+++ Python/10844.py
@@ -14,5 +14,10 @@ for i in range(1, 10):
 for i in range(2, n+1):
     for j in range(10):
         if j == 0:
-            dp[i][j] = dp[i-1][j] % mod
-        elif j == 9
+            dp[i][j] = dp[i-1][j+1] % mod
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1] % mod
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] % mod
+
+print(sum(dp[n]) % mod)
--- Python/test.py
+++ Python/test.py
@@ -1,12 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-n = input().strip().split('-')
-res = 0
-for i in n[0].split('+'):
-    res += int(i)
-for i in n[1:]:
-    for j in i.split('+'):
-        res -= int(j)
-
-print(res)
+n = int(input())
+dp = [0] * (n+1)
+dp[1] = 9
+if n >= 2:
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] * 2 - (i-1)
+print(dp[n]%1000000000)
