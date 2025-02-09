afd52d3470ff1cba1bb038c45813c0dc1cfcafc7
subakpup <129526142+subakpup@users.noreply.github.com>
2025-02-09 16:12:01 +0900

    250207

--- Python/30804.py
+++ Python/30804.py
@@ -6,3 +6,15 @@ arr = list(map(int,input().split()))
 cnt = {}
 left,res = 0,0
 
+for right in range(n):
+    cnt[arr[right]] = cnt.get(arr[right], 0) + 1
+
+    while len(cnt) > 2:
+        cnt[arr[left]] -= 1
+        if cnt[arr[left]] == 0:
+            del cnt[arr[left]]
+        left += 1
+
+    res = max(res, right - left + 1)
+
+print(res)
--- Python/test.py
+++ Python/test.py
@@ -1,10 +1,10 @@
-import sys
-input = sys.stdin.readline
+cnt = {}
 
-n = int(input())
-dp = [0] * (n+1)
-dp[1] = 9
-if n >= 2:
-    for i in range(2, n+1):
-        dp[i] = dp[i-1] * 2 - (i-1)
-print(dp[n]%1000000000)
+for i in range(int(input())):
+    a,b = map(int,input().split())
+    if a not in cnt:
+        cnt[a] = b
+    else:
+        cnt[a] += b
+    
+print(cnt)
