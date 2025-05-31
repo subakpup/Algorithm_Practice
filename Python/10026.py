15e336f3058fd367f342f4ee40d1edb8299a3c78
subakpup <129526142+subakpup@users.noreply.github.com>
2025-05-31 19:33:38 +0900

    250531

--- Python/10026.py
+++ Python/10026.py
@@ -2,25 +2,38 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(a,b,color,pic,visited):
+def bfs(graph, visited, a, b):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
     queue = deque([(a,b)])
     visited[a][b] = True
     
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    
     while queue:
         x,y = queue.popleft()
+        
         for i in range(4):
-            nx,ny = x+dx[i],y+dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if pic[nx][ny] == 
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and graph[x][y] == graph[nx][ny]:
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+    
+    return 1
+
+def counting(graph):
+    visited = [[False] * n for _ in range(n)]
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                cnt += bfs(graph, visited, i, j)
+    
+    return cnt
 
 if __name__ == "__main__":
     n = int(input())
+    RGB = [list(input().rstrip()) for _ in range(n)]
+    RB = [['R' if r == 'G' else r for r in row] for row in RGB]        
     
-    pic = [list(input()) for _ in range(n)]
-    visited = [[False] * n for _ in range(n)]
-    
-    pic_blind = [[i if i == "B" else "G" for i in r] for r in pic]
-    visited_blind = [[False] * n for _ in range(n)]
+    print(counting(RGB), counting(RB))
