4039af6732920ccc80906392ff44812a5f7d11f4
subakpup <129526142+subakpup@users.noreply.github.com>
2025-06-11 14:48:36 +0900

    250611

--- Python/15686.py
+++ Python/15686.py
@@ -1,11 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-def dfs():
+def distance(chicken):
     total = 0
+    for x1,y1 in h:
+        dist = 1e9
+        for x2,y2 in chicken:
+            dist = min(dist, abs(x1-x2) + abs(y1-y2))
+        total += dist
+    return total
+
+def dfs(s,d):
+    global answer
+    if d == m:
+        dist = distance(chicken)
+        answer = min(answer, dist)
+        return
     
-    for x,y in h:
-        
+    for i in range(s, len(ch)):
+        chicken.append(ch[i])
+        dfs(i+1, d+1)
+        chicken.pop()
 
 if __name__ == "__main__":
     n,m = map(int,input().split())
@@ -22,4 +37,7 @@ if __name__ == "__main__":
                 ch.append((i,j))
                 
     answer = 1e9
+    chicken = []
     
+    dfs(0,0)
+    print(answer)
