28cc59810c8464abc8fac6d2f9b5ba7da6f386dc
수박강아지 <129526142+subakpup@users.noreply.github.com>
2025-08-12 14:27:21 +0900

    250812

---
+++ Java/src/Main_10250.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_10250 {
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < t; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			
+			int answer = 101;
+			while (n - h > 0) {
+				answer += 1;
+				n -= h;
+			}
+			answer += (n-1) * 100;
+			sb.append(answer + "\n");
+		}
+		System.out.println(sb);
+	}
+
+}
