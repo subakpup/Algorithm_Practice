d838136da8460678c40c0d9da2430115618d0ab0
수박강아지 <129526142+subakpup@users.noreply.github.com>
2025-08-14 01:51:35 +0900

    250814

---
+++ Java/src/Main_2231.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_2231 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int answer = 0;
+		for (int i = 1; i < n; i++) {
+			int tmp = 0;
+			int num = i;
+			
+			while (num != 0) {
+				tmp += num % 10;
+				num /= 10;
+			}
+			
+			if (tmp + i == n) {
+				answer = i;
+				break;
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
