253146fe1605f8da9e37ed685046a05c32192884
subakpup <129526142+subakpup@users.noreply.github.com>
2025-10-02 00:01:52 +0900

    251001

---
+++ Java/src/Main_1271.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main_1271 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger n = new BigInteger(st.nextToken());
+		BigInteger m = new BigInteger(st.nextToken());
+		
+		System.out.println(n.divide(m) + "\n" + n.mod(m));
+	}
+
+}
