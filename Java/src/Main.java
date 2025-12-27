18327bf23841f2e2d49701e47de1e51bf8664473
subakpup <129526142+subakpup@users.noreply.github.com>
2025-12-27 16:30:21 +0900

    251226

---
+++ Java/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < n - i; j++) sb.append(' ');
+			for (int j = 0; j < 2 * i - 1; j++) sb.append('*');
+			sb.append('\n');
+		}
+		
+		for (int i = n - 1; i >= 1; i--) {
+			for (int j = 0; j < n - i; j++) sb.append(' ');
+			for (int j = 0; j < 2 * i - 1; j++) sb.append('*');
+			sb.append('\n');
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
