162ae69d7ab35c15bd11889ab369698659e439c1
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-03 23:34:39 +0900

    260202

---
+++ Java/src/Main_5339.java
@@ -0,0 +1,17 @@
+
+public class Main_5339 {
+
+	public static void main(String[] args) {
+		System.out.print("     /~\\\n"
+				+ "    ( oo|\n"
+				+ "    _\\=/_\n"
+				+ "   /  _  \\\n"
+				+ "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n"
+				+ "============\n"
+				+ "|          |\n"
+				+ "|          |\n"
+				+ "|          |");
+	}
+
+}
