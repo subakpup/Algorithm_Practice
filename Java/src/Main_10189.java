65ce01ac8439d4a70e8654360dfa1fe98e8a4de9
subakpup <hjsu0825@naver.com>
2026-02-14 10:30:00 +0900

    260213

---
+++ Java/src/Main_10189.java
@@ -0,0 +1,10 @@
+public class Main_10189 {
+
+	public static void main(String[] args) {
+		System.out.println("#  # #### #### #  #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#  # #### #### #  #");
+	}
+
+}
